e47e643db6636db97d7b4cc856d99d5e81886d3a
pablo <u22220256@utp.edu.pe>
2024-10-29 01:47:44 -0500

    v3.14

--- src/main/java/com/example/avance2_proyfinal/repository/ProductoRepository.java
+++ src/main/java/com/example/avance2_proyfinal/repository/ProductoRepository.java
@@ -4,4 +4,5 @@ import com.example.avance2_proyfinal.model.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductoRepository  extends JpaRepository<Producto, Integer> {
+    //aaaaaaaaaaaaaaaaaaaa
 }
